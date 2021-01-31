89d7894185fb5bc07e072e32b561192a38d443a9
Michał Pastewski <michpastewski@gmail.com>
2021-01-31 21:52:07 +0100

    added basic nav with router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, NavLink} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Meal from './pages/Meal';
+import Diaper from './pages/Diaper';
+import Sleep from './pages/Sleep';
 
 function App() {
   return (
-    <div>
-      <h1>babyDaily</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <header>
+          <nav>
+            <h1>babyDaily</h1>
+            <ul>
+              <li>
+                <NavLink to="/">home</NavLink>
+              </li>
+              <li>
+                <NavLink to="meal">karmienie</NavLink>
+              </li>
+              <li>
+                <NavLink to="diaper">pielucha</NavLink>
+              </li>
+              <li>
+                <NavLink to="sleep">sen</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/meal" component={Meal} />
+        <Route path="/diaper" component={Diaper} />
+        <Route path="/sleep" component={Sleep} />
+      </Switch>
+
+    </Router>
     );
 }
 
---
+++ src/pages/Meal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Meal = () => (
+   <section>
+      <h1>Posiłki</h1>
+   </section>
+);
+
+export default Meal;
